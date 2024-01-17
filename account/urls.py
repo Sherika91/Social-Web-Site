564f2f9c551b3cc3f1a04fef62fe0b9e19655d5d
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-17 18:04:43 +0300

    Urls change for Log-in&log-out

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import user_login
 
 
 urlpatterns = [
-    path('login/', user_login, name='login')
+    # Previous URL adress for Login
+    # path('login/', user_login, name='login'),
+
+    # Djangoss Log-in & Log-out URLs
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
