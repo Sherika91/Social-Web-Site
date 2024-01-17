0f5ac01bd2dff3ea1b8ce48a5fae0b9b819cf558
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-17 18:47:20 +0300

    Dashboard urls/views add

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import user_login
+from django.urls import path
 
+from . import views
 
 urlpatterns = [
     # Previous URL adress for Login
-    # path('login/', user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
 
     # Djangoss Log-in & Log-out URLs
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Dashboard URLs
+    path('', views.dashboard, name='dashboard'),
 ]
