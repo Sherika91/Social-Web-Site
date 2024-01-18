9260a80b591582de7f3ba1ab64f4a536a9433dcc
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-18 12:15:45 +0300

    Add password change urls/templates

--- account/urls.py
+++ account/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    # Change Password URLs
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
     # Dashboard URLs
     path('', views.dashboard, name='dashboard'),
 ]
