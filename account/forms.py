9bdd6f815b4e36affefc7ea5772a53ff7f4ab5b4
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-19 11:47:44 +0300

    Add Email checking while registering and editing profile, user will get error if email is already used.

--- account/forms.py
+++ account/forms.py
@@ -27,12 +27,27 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
 
+    def clean_email(self):
+        """ Check if email is already in use when registering """
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     """ User edit profile form """
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        """ Check if email is already in use when editing profile """
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
