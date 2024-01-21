b8240d0ce0a2df25d49813940a8624da15933279
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-21 11:23:01 +0300

    Model name fix

--- images/models.py
+++ images/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.text import slugify
 
 
-class Imege(models.Model):
+class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
