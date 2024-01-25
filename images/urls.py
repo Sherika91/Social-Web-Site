3c58a8b29c3a2ea12342a741b49c29d6d4a04aa5
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-25 19:56:48 +0300

    Add Image like functionality url

--- images/urls.py
+++ images/urls.py
@@ -7,5 +7,5 @@ app_name = ImagesConfig.name
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
-
+    path('like/', views.image_like, name='like'),
 ]
