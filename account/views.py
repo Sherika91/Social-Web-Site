72af6ced25a30e639d09bc0dc617e2e080a27101
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-26 14:16:38 +0300

    Add User List and user detail func

--- account/views.py
+++ account/views.py
@@ -1,11 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
 from .models import Profile
-from django.contrib import messages
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -81,3 +84,21 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                  'account/user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+@login_required
+def use_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
