841bb84df62128c14c1e8020b7d1b92c2b5ecf6e
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-31 10:43:49 +0300

    Add User Follow URl, deleted comments

--- account/urls.py
+++ account/urls.py
@@ -4,33 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # Previous URL adress for Login
-    # path('login/', views.user_login, name='login'),
-
-    # Djangoss Log-in & Log-out URLs
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    #
-    # # Password Change URLs
-    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    #
-    # # Password Reset URLs
-    # path('password-reset/',
-    #      auth_views.PasswordResetView.as_view(),
-    #      name='password_reset'),
-    #
-    # path('password-reset-done/',
-    #      auth_views.PasswordResetDoneView.as_view(),
-    #      name='password_reset_done'),
-    #
-    # path('password-reset/<uidb64>/<token>/',
-    #      auth_views.PasswordResetConfirmView.as_view(),
-    #      name='password_reset_confirm'),
-    #
-    # path('password-reset/complete/',
-    #      auth_views.PasswordResetCompleteView.as_view(),
-    #      name='password_reset_complete'),
 
     # Djangos's All Auth URLs, All the ones above.
     path('', include('django.contrib.auth.urls')),
@@ -47,4 +20,7 @@ urlpatterns = [
     # User List URL's
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.use_detail, name='user_detail'),
+
+    # User Follows URL's
+    path('users/follow/', views.user_follow, name='user_follow'),
 ]
